3bb47b9f15ebb798251ce604809fabf6f364ccaa
wallaceiam <chris@mymojo.ca>
2019-03-10 17:36:27 +0000

    Refactoring and adding Virgin Business Money conversions

---
+++ src/store/handlers.js
@@ -0,0 +1,88 @@
+export const StarlingHandler = {
+    canConvert: header => {
+        return !(
+            header["Date"] === undefined ||
+            header["Counter Party"] === undefined ||
+            header["Reference"] === undefined ||
+            header["Amount (GBP)"] === undefined ||
+            header["Type"] === undefined ||
+            header["Balance (GBP)"] === undefined
+        );
+    },
+    convert: resultSet => {
+        return resultSet
+            .filter(x => x.Date !== null)
+            .map(x => {
+                // Date, Counter Party, Reference, Type, Amount (GBP), Balance (GBP)
+                const date = x["Date"];
+                const counterParty = x["Counter Party"];
+                const reference = x["Reference"];
+                const amount = x["Amount (GBP)"];
+                const balance = x["Balance (GBP)"];
+                return {
+                    Date: date,
+                    Description:
+                        reference && reference.length > 0
+                            ? `${counterParty} (${reference.replace(/(\s\s+)/g, " ")})`
+                            : counterParty,
+                    "Paid In": amount > 0 ? amount : undefined,
+                    "Paid Out": amount < 0 ? amount * -1 : undefined,
+                    Balance: balance
+                };
+            });
+    }
+};
+
+export const VirginBusinessHandler = {
+    canConvert: header => {
+        return !(
+            header["Date"] === undefined ||
+            header["Details"] === undefined ||
+            header["Money out"] === undefined ||
+            header["Money in"] === undefined ||
+            header["Balance"] === undefined
+        );
+    },
+    convert: resultSet => {
+        return resultSet
+            .filter(x => x.Date !== null)
+            .map(x => {
+                // Date	Details	Money out	Money in	Balance
+                const date = x["Date"];
+                const details = x["Details"];
+                const inflow = x["Money in"];
+                const outflow = x["Money out"];
+                const balance = x["Balance"];
+                return {
+                    Date: date,
+                    Description: details,
+                    "Paid In": inflow || undefined,
+                    "Paid Out": outflow || undefined,
+                    Balance: balance
+                };
+            });
+    }
+};
+
+
+export const cleanizeContents = (csv) => {
+    const lines = csv.split('\n');   // lines is an array of strings
+
+    var newCsv = '';
+    var readDate = false;
+    // Loop through all lines
+    for (var i = 0; i < lines.length; i++) {
+        const line = lines[i] || '';
+        if(readDate && line.length > 0 && line.trimEnd() !== ',,,,') {
+            newCsv += `\n${line}`;
+        } else {
+            readDate = false;
+        }
+        if(line.includes('Date,')) {
+            readDate = true;
+            newCsv = `${line}`;
+        }
+        
+    }
+    return newCsv;
+};
--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,16 @@ import Papa from 'papaparse';
 
 import { upload } from '../services/file-upload.service';
 
+import { StarlingHandler, VirginBusinessHandler, cleanizeContents } from './handlers';
+
 Vue.use(Vuex);
 
 const STATUS_INITIAL = 0;
 const STATUS_SAVING = 1;
 const STATUS_SUCCESS = 2;
 const STATUS_FAILED = 3;
+
+const handlers = [ StarlingHandler, VirginBusinessHandler ];
 
 export default new Vuex.Store({
     state: {
@@ -73,7 +77,7 @@ export default new Vuex.Store({
             upload(formData)
                 .then(x => new Promise((resolve, reject) => {
                     const file = x[0];
-                    const csv = file.id;
+                    const csv = cleanizeContents(file.id);
                     const fileName = file.fileName;
                     var results = Papa.parse(csv, {
                         header: true,
@@ -81,30 +85,15 @@ export default new Vuex.Store({
                     });
                     const resultSet = (results.data || []);
                     const first = resultSet.length > 0 ? resultSet[0] : null;
-                    if (first['Date'] === undefined || first['Counter Party'] === undefined ||
-                        first['Reference'] === undefined || first['Amount (GBP)'] === undefined ||
-                        first['Type'] === undefined || first['Balance (GBP)'] === undefined) {
+                    const handler = handlers.find(x => x.canConvert(first));
+                    if(!handler) {
                         reject(new Error('Incorrect headers for format'));
                         return;
                     }
-                    const data = resultSet
-                        .filter(x => x.Date !== null)
-                        .map(x => {
-                            // Date, Counter Party, Reference, Type, Amount (GBP), Balance (GBP)
-                            const date = x['Date'];
-                            const counterParty = x['Counter Party'];
-                            const reference = x['Reference'];
-                            const amount = x['Amount (GBP)'];
-                            const balance = x['Balance (GBP)'];
-                            return {
-                                Date: date,
-                                Description: reference && reference.length > 0
-                                    ? `${counterParty} (${reference.replace(/(\s\s+)/g, ' ')})`
-                                    : counterParty,
-                                'Paid In': amount > 0 ? amount : undefined,
-                                'Paid Out': amount < 0 ? amount * -1 : undefined,
-                                Balance: balance
-                            };
+                    
+                    const data = handler.convert(resultSet)
+                        .sort((a, b) => {
+                            return (new Date(b.Date) - new Date(a.Date)) * -1;
                         });
 
                     commit('uploadSuccess', { x, data, fileName: fileName.replace('Starling', 'inniAccount') });
