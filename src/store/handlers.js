236d1849d4591ddab604865d20262343a1b79d39
Chris Wallace <chris@mymojo.ca>
2019-09-01 10:24:30 +0100

    Rebuild

--- src/store/handlers.js
+++ src/store/handlers.js
@@ -38,7 +38,7 @@ export const StarlingHandler = {
 const parseDateStr = date => {
   const parts = date.split("/");
   return new Date(Number(parts[2]), Number(parts[1]) - 1, Number(parts[0]));
-};
+}; 
 
 export const VirginBusinessHandler = {
   canConvert: header => {
